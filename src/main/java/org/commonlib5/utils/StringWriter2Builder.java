58bb2f5eaeb20396cf1f88440e5ae54b1369874b
Nicola De Nisco <nicola.denisco@dedalus.eu>
2025-08-29 12:45:54 +0200

    rifiniture

---
+++ src/main/java/org/commonlib5/utils/StringWriter2Builder.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025 Nicola De Nisco
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.commonlib5.utils;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Uno StringWriter che scrive in uno StringBuilder.
+ *
+ * @author Nicola De Nisco
+ */
+public class StringWriter2Builder extends Writer
+{
+  private final StringBuilder sb;
+
+  public StringWriter2Builder(StringBuilder sb)
+  {
+    this.sb = sb;
+  }
+
+  @Override
+  public void write(char[] cbuf, int off, int len)
+     throws IOException
+  {
+    sb.append(cbuf, off, len);
+  }
+
+  @Override
+  public void flush()
+     throws IOException
+  {
+  }
+
+  @Override
+  public void close()
+     throws IOException
+  {
+  }
+}
